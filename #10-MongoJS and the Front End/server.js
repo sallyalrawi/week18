94bd1b1420c4f10764d4c612321fdcf95c002ae0
sally <sallyakrum83@gmail.com>
2019-04-09 21:37:05 -0500

    Activity-10

---
+++ #10-MongoJS and the Front End/server.js
@@ -0,0 +1,103 @@
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+
+// Set up a static folder (public) for our web app
+app.use(express.static("public"));
+
+// Database configuration
+var databaseUrl = "zoo";
+var collections = ["animals"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// TODO: make two more routes
+
+// Route 1
+// =======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
+app.get("/all", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything
+  db.animals.find({}, function(error, found) {
+    // Log any errors if the server encounters one
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the result of this query to the browser
+    else {
+      res.json(found);
+    }
+  });
+});
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
+
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+// 
+// 3. At the "/name" path, display every entry in the animals collection, sorted by name
+app.get("/name", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything,
+  // but this time, sort it by name (1 means ascending order)
+  db.animals.find().sort({ name: 1 }, function(error, found) {
+    // Log any errors if the server encounters one
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the result of this query to the browser
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
+app.get("/weight", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything,
+  // but this time, sort it by weight (-1 means descending order)
+  db.animals.find().sort({ weight: -1 }, function(error, found) {
+    // Log any errors if the server encounters one
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the result of this query to the browser
+    else {
+      res.json(found);
+    }
+  });
+});
+// Listen on port 3000
+app.listen(3002, function() {
+  console.log("App running on port 3002!");
+});
